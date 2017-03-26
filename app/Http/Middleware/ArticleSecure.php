1b09605f45a2726cbf729d9808cf6c04aa617fa4
narmin <narmin_2011@mail.ru>
2017-03-26 20:35:06 +0300

    start middleware

---
+++ app/Http/Middleware/ArticleSecure.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ArticleSecure
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->user() ){
+
+        }
+
+
+        return $next($request);
+    }
+}
